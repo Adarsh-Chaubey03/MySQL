6d10813623cdf8d94fd90d2e78c5b5e113859bd8
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2026-01-16 21:19:51 +0530

    Day-0-SQL

---
+++ 01 Basics of MySQL/01_database_and_table_basics.sql
@@ -0,0 +1,37 @@
+-- 1. Create a database
+CREATE DATABASE starterSQL;
+
+-- 2. Select the database for use
+USE starterSQL;
+
+
+-- 3. Create a table named users
+CREATE TABLE users (
+id INT AUTO_INCREMENT PRIMARY KEY,                   -- unique user ID
+name VARCHAR(100) NOT NULL,                          -- user name (required)
+email VARCHAR(100) UNIQUE NOT NULL,                  -- unique email (required)
+gender ENUM('Male', 'Female', 'Other'),              -- allowed gender values
+date_of_birth DATE,                                  -- date only (YYYY-MM-DD)
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP       -- auto time stamp
+);
+
+
+
+-- 4. View all records from users table 
+SELECT * FROM users;
+
+
+-- 5. View only specific columns
+SELECT name, email FROM users;
+
+
+-- 6. Rename table users to customers
+RENAME TABLE users TO customers;
+
+-- 7. Rename table customers back to users
+RENAME TABLE customers TO users;
+
+
+
+-- 8. Delete the database (use with caution)
+DROP DATABASE startersql;
---
+++ 01 Basics of MySQL/02_insert_records.sql
@@ -0,0 +1,15 @@
+use starterSQL;
+
+-- Insert a single user record
+INSERT INTO users (name, email, gender, date_of_birth) VALUES
+('Bob', 'bob@example.com', 'Male', '1990-11-23');
+
+
+-- Insert multiple user records at once
+INSERT INTO users (name, email, gender, date_of_birth) VALUES
+('Alice', 'alice@example.com', 'Female', '1990-11-23'),
+('Charlie', 'charlie@example.com', 'Other', '1988-02-17');
+
+
+-- Verify inserted data
+SELECT * FROM users;
